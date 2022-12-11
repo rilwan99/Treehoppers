926910d8b2670067d1c1c81822657e7a8be2a30e
AvranWong <avranwong@gmail.com>
2022-12-11 15:15:59 +0800

    Created app.js and updated mint_bot.py
    Created express js boiler plate and updated mint_bot to interact with express api

--- ui/mint_bot.py
+++ ui/mint_bot.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     Application,
@@ -8,6 +9,12 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
+from dotenv import load_dotenv
+import os
+
+#Import Environment Vairables
+load_dotenv()
+TELE_API = os.getenv('TELE_API')
 
 # Enable logging
 logging.basicConfig(
@@ -60,7 +67,7 @@ async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
     await update.message.reply_text(
         "Looks great! Now, send me your wallet address, title, and description for your NFT in the following format:\n\n"
-        "address : title : description"
+        "address : title : symbol"
     )
 
     return METADATA
@@ -69,21 +76,50 @@ async def metadata(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
     user = update.message.from_user
     user_response = update.message.text
+    # Formats users response
     address = user_response.split(' : ')[0]
     context.user_data['address'] = address
     chain = context.user_data['chain']
     title = user_response.split(' : ')[1]
-    description = user_response.split(' : ')[2]
+    symbol = user_response.split(' : ')[2]
 
     logger.info(
-        "address of %s: %s, nft title: %s, nft description: %s", user.first_name, address, title, description
+        "address of %s: %s, nft title: %s, nft symbol: %s", user.first_name, address, title, symbol
     )
 
     await update.message.reply_text(
         f"Awesome, now Minting NFT to {address} on {chain}"
     )
 
-    # insert contract call to mint NFT, and once minted return success message
+    # Post request to Express API
+    # Set the endpoint URL, local for now
+    endpoint_url = "http://localhost:3000/"
+
+    # Set the parameters
+    # Default params for testing
+    params = {
+        "publicKey": "5BQ2t7HdGEQ2c1XD3SJgCsy7f9Pf5JkpLp1gM1V7FAnQ",
+        "title": "My NFT",
+        "symbol": "MYNFT",
+    }
+    # params = {
+    #     "publicKey": address,
+    #     "title": title,
+    #     "symbol": symbol,
+    # }
+
+    # Send the request
+    response = requests.post(endpoint_url, json=params)
+
+    # Check the response
+    if response.status_code == 200:
+        print("API call successful!")
+        print(response)
+        await update.message.reply_text(
+        f"NFT Minted! You can view your NFT here: {address}"
+        )
+    else:
+        print("Error calling API: {}".format(response.text))
 
     reply_keyboard = [["Yes", "No"]]
 
@@ -129,7 +165,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("YOUR_TELE_BOT_TOKEN").build()
+    application = Application.builder().token(TELE_API).build()
 
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
     conv_handler = ConversationHandler(
