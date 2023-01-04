50330349698ca2b94e321ad14a2e9133ac5e55bc
Liam Wells Ayathan <ayathan21@gmail.com>
2023-01-04 15:22:06 +0800

    added claim functionality

--- ui/bots/older_bot.py
+++ ui/bots/older_bot.py
@@ -24,19 +24,19 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-ADJUST, CHAIN, PHOTO, METADATA, MINT, END = range(6)
+CHAIN, PHOTO, METADATA, MINT, END = range(5)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text(
         "/mint - Allows you to mint new coupons \n"
-        "/claim - Allows you to see your current coupons \n"
+        "/redeem - Allows you to see your current coupons \n"
         "/help - to see helpful commands",
         )
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text(
         "/mint - Allows you to mint new coupons \n"
-        "/claim - Allows you to see your current coupons \n"
+        "/redeem - Allows you to see your current coupons \n"
         "/help - to see helpful commands",
         )
 
@@ -63,14 +63,71 @@ async def mint(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         )
     else:
         print(response.json()['error'])
-    
+
+
+
     return CHAIN
 
+# async def upload(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    
+#     # Stores the selected chain and asks for the NFT Photo
+#     user = update.message.from_user
+#     chain = update.message.text
+#     context.user_data['chain'] = chain
+#     logger.info("Coupon Selected by %s: %s", user.first_name, chain)
+    
+#     await update.message.reply_text(
+#         "Nice! please send me the NFT that you would like to mint: ",
+#         reply_markup=ReplyKeyboardRemove(),
+#     )
+
+#     return PHOTO
+
+
+# async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    
+#     # Stores photo and prompts for metadata
+#     user = update.message.from_user
+#     photo_file = await update.message.photo[-1].get_file()
+    
+#     await photo_file.download_to_drive(f"nft_photos/{user.first_name}_nft.jpg")
+#     logger.info("nft of %s: %s", user.first_name, f"{user.first_name}_nft.jpg")
+
+#     # Send request to upload meta data and image
+#     logger.info(
+#         "Uploading Image..."
+#     )
+
+#     # Create path where the image is located
+#     base_path = os.path.realpath(__file__)
+#     parent_dir = os.path.dirname(base_path)
+#     image_path = os.path.join(parent_dir, 'nft_photos/'+user.first_name+"_nft.jpg")
+#     json = {
+#     "image_path":image_path,
+#     }
+#     # Send image_path to endpoint
+#     endpoint_url = "http://localhost:3000"
+#     response = requests.post(endpoint_url+"/uploadImage", json = json)
+     
+#     # Construct URI
+#     image_CID = response.text
+#     logger.info(
+#         "Image uploaded at %s!",image_CID
+#     )
+    
+#     await update.message.reply_text(
+#         "Looks great! Now, send me the title and symbol for your NFT in the following format:\n\n"
+#         "title : symbol"
+#     )
+
+#     return METADATA
+
 async def chain(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+
     user = update.message.from_user
-    collection = update.message.text # this is where the option from the reply keyboard goes - grab sheng shiong etc
-    context.user_data['collection'] = collection
-    logger.info("Coupon Selected by %s: %s", user.first_name, collection)
+    chain = update.message.text # this is where the option from the reply keyboard goes - grab sheng shiong etc
+    context.user_data['chain'] = chain
+    logger.info("Coupon Selected by %s: %s", user.first_name, chain)
 
     # user id and user handle that will be added to the firebase db later under users
     user_id = update.effective_user.id
@@ -112,7 +169,26 @@ async def chain(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     print(walletStatus)
 
     context.user_data['address'] = address
-    collection = context.user_data['collection']
+    chain = context.user_data['chain']
+    # title = user_response.split(' : ')[0]
+    # symbol = user_response.split(' : ')[1]
+
+    # Send request to upload meta data and image
+    # logger.info(
+    #     "Uploading Image..."
+    # )
+
+    # Create path where the image is located
+    # base_path = os.path.realpath(__file__)
+    # parent_dir = os.path.dirname(base_path)
+    # image_path = os.path.join(parent_dir, 'nft_photos/'+user.first_name+"_nft.jpg")
+    # json = {
+    # "image_path":image_path,
+    # }
+    # Send image_path to endpoint
+    # response = requests.post(endpoint_url+"/uploadImage", json = json)
+     
+    # Construct URI
     image_CID = response.text
     logger.info(
         "Image uploaded at %s!",image_CID
@@ -134,7 +210,9 @@ async def chain(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     logger.info(
         "Metadata uploaded at %s!",metadata_CID
     )
-
+    # logger.info(
+    #     "address of %s: %s, nft title: %s, nft symbol: %s, ImageCID: %s, MetaDataCID: %s", user.first_name, address, title, symbol, image_CID, metadata_CID
+    # )
     if walletStatus == True:
       await update.message.reply_text(
           f"You don't have a wallet! Creating and minting the NFT to your new wallet: {address}"
@@ -144,19 +222,16 @@ async def chain(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
           f"You have an existing wallet! Now minting the NFT to your wallet: {address}"
       )
 
-    print(f'this is the collection being minted - {collection} - information should be retrieved from firebase through the mint endpoint')
-
     params = {
         "publicKey": keys['publicKey'],
         "privateKey": keys['privateKey'],
-        "MerchantId": collection,
         # "title": title,
         # "symbol": symbol,
         "metadata": metadata_CID,
     }
 
     # Send the request to mint
-    response = requests.post(endpoint_url + "/mint", json=params) # need to fix, sending information without waiting for transaction to go through
+    response = requests.post(endpoint_url + "/mint", json=params)
 
     # Check the response
     if response.status_code == 200:
@@ -184,7 +259,7 @@ async def chain(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
     return END
 
-async def claim(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def redeem(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
     # user id and user handle that will be added to the firebase db later under users
     user_id = update.effective_user.id
@@ -193,7 +268,7 @@ async def claim(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     # user_response = update.message.text
 
     await update.message.reply_text(
-        f"Awesome, give us a moment to check if you have any Coupons available to claim"
+        f"Awesome, give us a moment to check if you have any Coupons available to redeem"
     )
     logger.info(
         "Checking Records..."
@@ -235,71 +310,21 @@ async def claim(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         "publicKey": address,
     }
 
-    response = requests.post(endpoint_url+"/coupons", json = params) #checking what coupons he has
+    response = requests.post(endpoint_url+"/coupons", json = params)
 
     # check the response status code
     if response.status_code == 200:
         # print the public and private keys
         nfts = response.json()
         # result = keys['output']
-        nft_dict = {}
-        for nft in nfts:
-          # must take into account the expired fieldfor 
-          name = nft['name']
-          mint_address = nft['mintAddress']
-          if name in nft_dict:
-            nft_dict[name]['count'] += 1 #increasing the count with every similar NFT
-          else:
-            nft_dict[name] = {'mint_address':mint_address,'count':1}
-          # you have 16 grab nfts, now you have 15 etc
-        reply_keyboard = [list(nft_dict.keys())]
-        reply_string = ''
-        print('--------------------')
-        print(reply_keyboard)
-        print('--------------------')
-        context.user_data['nft_dict'] = nft_dict #very important to keey the information
-
-        for name,output in nft_dict.items():
-          count = output['count']
-          reply_string += f'{count} {name} coupons \n'
-
-        if reply_string == '': # all the coupons have been expired
-          await update.message.reply_text(
-              "You do not have any available coupons"
+        await update.message.reply_text(
+              f"{nfts}"
           )
-
-        else:
-          await update.message.reply_text(
-              "You have available coupons! You currently have: \n\n"
-              f'{reply_string}',
-              reply_markup=ReplyKeyboardMarkup(
-                  reply_keyboard, one_time_keyboard=True, input_field_placeholder="ShengSiong/Grab?"
-              ),)
-          print(context.user_data['nft_dict'])
-
     else:
         # print the error message
         print(response.json()['error'])
 
-    return ADJUST
-
-async def adjust(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    coupon = update.message.text
-    user_id = update.effective_user.id
-    logger.info("Coupon Selected by %s: %s", f'{coupon}')
-    nft_dict = context.user_data['nft_dict'] #stores as a global variable from claim
-    mint_address = nft_dict[coupon]['mint_address']
-
-    params_new = {
-        "id": user_id,
-        "coupon": coupon,
-        "mint_address": mint_address
-    }
-
-    endpoint_url = "http://localhost:3000"
-
-    response = requests.post(endpoint_url+"/claim", json=params_new)
-
+    response = requests.post(endpoint_url+"/redeem", json=params_key)
     if response.status_code == 200:
         result = response.json()
         await update.message.reply_text(
@@ -339,6 +364,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
     return ConversationHandler.END
 
+
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
@@ -350,12 +376,11 @@ def main() -> None:
             CommandHandler("start", start),
             CommandHandler("help", help),
             CommandHandler("mint", mint),
-            CommandHandler("claim",claim), 
+            CommandHandler("redeem",redeem), 
             # CommandHandler("upload", upload)
             ],
         states={
-            ADJUST: [MessageHandler(filters.Regex("^(ShengSiong|Grab)$"), adjust)],
-            CHAIN: [MessageHandler(filters.Regex("^(Grab)$"), chain)],
+            CHAIN: [MessageHandler(filters.Regex("^(ShengSiong|Grab|NTUC)$"), chain)],
             # PHOTO: [MessageHandler(filters.PHOTO, photo)],
             # METADATA: [MessageHandler(filters.Regex("^(.*):(.*)"), metadata)],
             END: [MessageHandler(filters.Regex("^(Yes|No)$"), end)],
